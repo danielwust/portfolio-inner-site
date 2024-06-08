173dc9a159b15ad997a624f83f9f86eb585187ea
Daniel Wust <danielwust99@gmail.com>
2024-06-08 13:24:32 -0300

    some fix for router

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MemoryRouter } from 'react-router';
+// import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <MemoryRouter>
+            {/* <Router> */}
+                <App />
+            {/* </Router>         */}
+        </MemoryRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
